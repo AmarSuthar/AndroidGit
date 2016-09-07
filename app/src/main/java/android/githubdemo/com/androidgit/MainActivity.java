2016bb778afcd2126d92ab1f05414867c7cbb703
AmarSuthar <rad.amarsuthar@gmail.com>
2016-09-07 11:16:37 +0530

    buuton created

--- app/src/main/java/android/githubdemo/com/androidgit/MainActivity.java
+++ app/src/main/java/android/githubdemo/com/androidgit/MainActivity.java
@@ -1,10 +1,11 @@
 package android.githubdemo.com.androidgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
